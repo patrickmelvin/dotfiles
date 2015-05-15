888992fc8a50128423c1e863ad3929223a7a89cb
Patrick Melvin <patrick.melvin@sage.com>
2015-05-15 10:18:35 +0100

    Add scripts to setup and delink dotfiles

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# This script will sets up the dotfiles
+
+ln -nfs ./vim/ ~/.vim
+ln -nfs ./vimrc ~/.vimrc
+ln -nfs ./bash_profile ~/.bash_profile
+ln -nfs ./bash/ ~/.bash
+ln -nfs ./gitconfig ~/.gitconfig
